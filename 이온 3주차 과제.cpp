4d5a4bc8cabd2a59e7b5d196ece7baeb3424ab95
jbjoon10 <101044407+jbjoon10@users.noreply.github.com>
2022-03-16 23:08:32 +0900

    Update 이온 3주차 과제.cpp

--- 이온 3주차 과제.cpp
+++ 이온 3주차 과제.cpp
@@ -1,17 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-	int count = 0;
-	int result = 0;
-	int i;
+	int count1, count2, count3, count4, count5, count6;
+	int result;
 
-	for (i = 0; i < 6; i++)
 	{
-		count = int(printf("6개의 수를 입력하세요 : "));
-		scanf("%d", &count);
-			result += count;
+		printf("6개의 수를 입력하세요 : ");
+		scanf("%d, %d, %d, %d, %d, %d", &count1, &count2, &count3, &count4, &count5, &count6);
+			result = count1+ count2+ count3+ count4+ count5+ count6;
 	}
 	printf("%d", result);
 }
